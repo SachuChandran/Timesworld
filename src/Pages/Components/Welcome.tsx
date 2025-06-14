d93b2847b6dd67a4727c74bcb0d4e2316ab41388
SachuChandran <sachuvdr@gmail.com>
2025-06-14 16:56:02 +0530

    push login success completed final

--- src/Pages/Components/Welcome.tsx
+++ src/Pages/Components/Welcome.tsx
@@ -6,13 +6,13 @@ const Welcome = () => {
           {/* <div className='flex items-center w-full gap-5'> */}
             <Row gutter={5}>
                 <Col xs={24} md={8} lg={10}>
-                    <div className='flex-1 border-t-[3px] border-[#3D3D3D] h-[36px]' />
+                    <div className='welcome-border flex-1 border-t-[3px] border-[#3D3D3D] h-[36px]' />
                 </Col>
                 <Col xs={24} md={8} lg={4}>
                     <p className='text-[32px] font-[700] text-[#3D3D3D] uppercase text-center'>Welcome</p>
                 </Col>
                 <Col xs={24} md={8} lg={10}>
-                   <div className='flex-1 border-b-[3px] border-[#3D3D3D] h-[36px]' />
+                   <div className='welcome-border flex-1 border-b-[3px] border-[#3D3D3D] h-[36px]' />
                 </Col>
             </Row>
             {/* <div className='flex-1 border-t-[3px] border-[#3D3D3D] h-[22px]' />
